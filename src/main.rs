7a8871ea163fca5d52f6d75147f24ba9d5064b25
Padrition <padrition@gmail.com>
2021-06-29 13:14:07 +0600

    Using references to avoid ownership problems

--- src/main.rs
+++ src/main.rs
@@ -3,28 +3,44 @@ enum StatusMessage{
     Ok,
 }
 
+struct GroundStation;
+
+impl GroundStation{
+    fn send(&self, to: &mut CubeSat, msg: Message) {
+        to.mailbox.messages.push(msg);
+    }
+}
+
 #[derive(Debug)]
 struct CubeSat{
     id: u64,
+    mailbox: Mailbox,
 }
 
-fn chech_status(sat_id: CubeSat) -> StatusMessage{
-    StatusMessage::Ok
+impl CubeSat{
+    fn recv(&mut self) -> Option<Message>{
+        self.mailbox.messages.pop()
+    }
 }
+
+#[derive(Debug)]
+struct Mailbox{
+    messages: Vec<Message>,
+}
+
+type Message = String;
 
 fn main() {
-    let sat_a = CubeSat{ id: 0};
-    let sat_b = CubeSat{ id: 1};
-    let sat_c = CubeSat{ id: 2};
-    
-    let a_status = chech_status(sat_a);
-    let b_status = chech_status(sat_b);
-    let c_status = chech_status(sat_c);
-    println!("a: {:?}, b:{:?}, c:{:?}.", a_status, b_status,c_status);
-    
-    //waiting...
-    let a_status = chech_status(sat_a);
-    let b_status = chech_status(sat_b);
-    let c_status = chech_status(sat_c);
-    println!("a: {:?}, b:{:?}, c:{:?}.", a_status, b_status,c_status);
+    let base = GroundStation {};
+    let mut sat_a = CubeSat{ id: 0, mailbox: Mailbox{messages: vec![]}};
+
+    println!("t0: {:?}", sat_a);
+    base.send(&mut sat_a, Message::from("Hello there!"));
+
+    println!("t1: {:?}", sat_a);
+
+    let msg = sat_a.recv();
+    println!("t2: {:?}", sat_a);
+
+    println!("msg: {:?}", msg);
 }
